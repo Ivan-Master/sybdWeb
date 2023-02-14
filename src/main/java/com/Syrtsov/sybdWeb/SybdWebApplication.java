2066f0e1e30bc77bbd330b4c881464d4d716e4a5
YodaB <ivansyrcov26@gmail.com>
2023-02-14 21:37:17 +0300

    Это стартовый коммит

---
+++ src/main/java/com/Syrtsov/sybdWeb/SybdWebApplication.java
@@ -0,0 +1,13 @@
+package com.Syrtsov.sybdWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SybdWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SybdWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Syrtsov/sybdWeb/SybdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Syrtsov.sybdWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SybdWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
